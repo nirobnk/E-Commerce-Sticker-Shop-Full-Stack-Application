e22fe645b12dbf621c41198fbe37bc26fd18966b
nirobnk <eg245158@engug.ruh.ac.lk>
2026-02-11 19:31:34 +0530

    csrf setup

--- backend/src/main/java/com/nirobnk/stickershop/security/StickershopSecurityConfig.java
+++ backend/src/main/java/com/nirobnk/stickershop/security/StickershopSecurityConfig.java
@@ -20,6 +20,8 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -40,7 +42,9 @@ public class StickershopSecurityConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http)
             throws Exception {
-        return http.csrf(csrfConfig -> csrfConfig.disable())
+        return http.csrf(csrfConfig -> csrfConfig.
+                        csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                        .csrfTokenRequestHandler(new CsrfTokenRequestAttributeHandler()))
                 .cors(corsConfig -> corsConfig.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests((requests) -> {
                             publicPaths.forEach(path ->
--- backend/src/main/java/com/nirobnk/stickershop/service/impl/ProfileServiceImpl.java
+++ backend/src/main/java/com/nirobnk/stickershop/service/impl/ProfileServiceImpl.java
@@ -1,5 +1,6 @@
 package com.nirobnk.stickershop.service.impl;
 
+import com.nirobnk.stickershop.dto.AddressDto;
 import com.nirobnk.stickershop.dto.ProfileRequestDto;
 import com.nirobnk.stickershop.dto.ProfileResponseDto;
 import com.nirobnk.stickershop.entity.Address;
@@ -47,7 +48,7 @@ public class ProfileServiceImpl implements IProfileService {
         return profileResponseDto;
     }
 
-    private Customer getAuthenticatedCustomer() {
+    public Customer getAuthenticatedCustomer() {
         Authentication authentication =SecurityContextHolder.getContext().getAuthentication();
         String email = authentication.getName();
         return customerRepository.findByEmail(email).
@@ -58,11 +59,9 @@ public class ProfileServiceImpl implements IProfileService {
         ProfileResponseDto profileResponseDto = new ProfileResponseDto();
         BeanUtils.copyProperties(customer, profileResponseDto);
         if (customer.getAddress() != null) {
-            profileResponseDto.setStreet(customer.getAddress().getStreet());
-            profileResponseDto.setCity(customer.getAddress().getCity());
-            profileResponseDto.setState(customer.getAddress().getState());
-            profileResponseDto.setPostalCode(customer.getAddress().getPostalCode());
-            profileResponseDto.setCountry(customer.getAddress().getCountry());
+            AddressDto addressDto = new AddressDto();
+            BeanUtils.copyProperties(customer.getAddress(), addressDto);
+            profileResponseDto.setAddress(addressDto);
         }
         return profileResponseDto;
     }
